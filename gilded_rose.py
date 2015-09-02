d17252b098c3b1f4a2148cd796a4d4d49c0e0746
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2015-09-02 23:15:27 +0200

    Handle quality < 0

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,9 +9,7 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                item.quality = item.quality - 1
             else:
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -24,15 +22,15 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     item.quality = item.quality + 1
             if item.quality > 50:
                 item.quality = 50
+            if item.quality < 0:
+                item.quality = 0
             if item.name == "Sulfuras, Hand of Ragnaros":
                 item.quality = 80
 
