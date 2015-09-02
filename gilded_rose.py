413c3104bde9e9a337d875f0f7c16462ed710c05
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2015-09-02 23:09:19 +0200

    remove quality < 50 tests and maximise quality to 50 except for Sulfuras

--- gilded_rose.py
+++ gilded_rose.py
@@ -13,15 +13,12 @@ class GildedRose(object):
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        item.quality = item.quality + 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -33,9 +30,11 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+                    item.quality = item.quality + 1
+            if item.quality > 50:
+                item.quality = 50
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = 80
 
 class Item:
     def __init__(self, name, sell_in, quality):
